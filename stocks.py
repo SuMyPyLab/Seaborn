54f07d8a86a8c7a168c26584d3d6f6e8398222f6
Coding with Sufian <83113554+SuMyPyLab@users.noreply.github.com>
2024-02-22 22:04:15 +0530

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,1 +1,9 @@
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+df = pd.read_csv("stock_prices.csv", index_col="Date")
+df.dropna(inplace=True)     # Drop the rows that have NaN values
+corr_df = df.corr()         # correlation matrix
+# that is, correlation coefficients of all the columns in a matrix format
+sns.heatmap(corr_df, annot=True, cmap='Blues')
+plt.show()
